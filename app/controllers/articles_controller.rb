61438787a4b078e8afcc760dfdeb2adb23fdc4b3
410AngelaVu <andjela1993@gmail.com>
2021-02-01 21:34:33 +0100

    Added Category model and controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,31 @@
 class ArticlesController < ApplicationController
+
+def index
+@articles = Article.all.order('created_at DESC')
+end
+
+def new
+@article = Article.new
+	end
+
+def create  
+@article = current_user.articles.build(article_params)
+if @article.save
+	redirect_to @article
+else
+	render 'new'
+end
+
+end
+
+def update
+@article = Article.find(params[:id])
+	end
+
+private
+
+def article_params
+params.require(:article).permit(:author_id, :image, :title, :text)
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'registrations' }
   root 'homes#index'
+  resources :articles
+  
 end
