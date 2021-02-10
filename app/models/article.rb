76b9e2eb6975c3c6893618244a4831a0fc20789f
410AngelaVu <andjela1993@gmail.com>
2021-02-10 16:55:38 +0100

    Added Like model and controller

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,5 +4,6 @@ belongs_to :author, foreign_key:'author_id', class_name: 'User'
 has_many :articles_categories
 has_and_belongs_to_many :categories, through: :articles_categories
 accepts_nested_attributes_for :articles_categories 
+has_one_attached :image
   validates_presence_of :title, :text
 end
