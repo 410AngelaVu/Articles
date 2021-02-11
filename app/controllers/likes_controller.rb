12fd8f4ad0ddcdc71bad71e37cb5d5c2a10a3906
410AngelaVu <andjela1993@gmail.com>
2021-02-11 15:55:18 +0100

    Added Like button to Article

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,18 +3,18 @@ class LikesController < ApplicationController
 def create
 @like = current_user.likes.new(article_id: params[:article_id])
 if @like.save
-	redirect_to articles_path, notice: 'You liked this article'
+	redirect_to article_path(params[:article_id]), notice: 'You liked this article'
 else
-	redirect_to articles_path, notice: "You can't like this article"
+	redirect_to article_path(params[:article_id]), notice: "You can't like this article"
 end
 end
 
 def destroy
 @like = Like.find_by(id: params[:id], user: current_user, article_id: params[:article_id])
 if @like.destroy
-	redirect_to articles_path, notice:'You dislike this article'
+	redirect_to article_path(params[:article_id]), notice:'You dislike this article'
 else
-	redirect_to articles_path, notice: 'You cannot dislike article that you did not like before.'
+	redirect_to article_path(params[:article_id]), notice: 'You cannot dislike article that you did not like before.'
 	end
 end
 
