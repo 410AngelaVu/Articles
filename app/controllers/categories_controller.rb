554c1d929b8e338c05fcee854224323bac9dd8a1
410AngelaVu <andjela1993@gmail.com>
2021-02-16 16:12:59 +0100

    Styled Sign Up and Log In form

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,25 +1,11 @@
 class CategoriesController < ApplicationController
-# before_action :set_category
+
 def index
  @categories = Category.all
 
    @articles = Article.all.order('created_at DESC')
 
 end
-
-# def new
-# @category = Category.new
-# @categories = Category.all
-# 	end
-
-# def create
-# @category = current_user.categories.build(category_params)
-# if @category.save
-# 	redirect_to @category
-# else
-# 	render 'new'
-# end
-# end
 
 def show
 @category = Category.includes(:articles).find(params[:id])
@@ -29,9 +15,6 @@ def show
 
 private
 
- # def set_category
- #      @category = Category.find(params[:id])
- #    end
 
 def category_params
 params.require(:category).permit(:name, :priority)
