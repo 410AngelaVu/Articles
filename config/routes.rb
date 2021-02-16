92982c76a9598fe0075aeb38cd73a8040121a99e
410AngelaVu <andjela1993@gmail.com>
2021-02-16 22:08:29 +0100

    Testing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'registrations' }
-  root 'homes#index'
+  root to: 'homes#index'
   resources :articles do
   	resources :likes, only: [:create, :destroy, :index]
   end
