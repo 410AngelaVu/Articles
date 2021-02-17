538cf83a3157c60c97d551e9d7397881974ccd3d
410AngelaVu <andjela1993@gmail.com>
2021-02-17 22:06:55 +0100

    Added unless statement to image_tag

--- Gemfile
+++ Gemfile
@@ -28,6 +28,7 @@ gem 'simple_form', '~> 5.0', '>= 5.0.3' # Use Active Storage variant
 gem 'punching_bag', '~> 0.7.0'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.4', require: false
+
 gem 'rubocop', '~> 1.10', require: false
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/helpers/homes_helper.rb
+++ app/helpers/homes_helper.rb
@@ -16,23 +16,26 @@ module HomesHelper
   end
 
   def first_displayed_image
-    image_tag(Category.find(1).articles.last.image, class: 'arti-img')
-  end
+    image_tag(Category.find(1).articles.last.image, class: 'arti-img') unless Category.find(1).articles.last.image.blank?
+      
+    end
 
   def second_displayed_image
-    image_tag(Category.find(2).articles.last.image, class: 'arti-img')
+    image_tag(Category.find(2).articles.last.image, class: 'arti-img') unless Category.find(2).articles.last.image.blank?
   end
 
   def third_displayed_image
-    image_tag(Category.find(3).articles.last.image, class: 'arti-img')
+    image_tag(Category.find(3).articles.last.image, class: 'arti-img') unless Category.find(3).articles.last.image.blank?
   end
 
   def fourth_displayed_image
-    image_tag(Category.find(4).articles.last.image, class: 'arti-img')
+    image_tag(Category.find(4).articles.last.image, class: 'arti-img') unless Category.find(4).articles.last.image.blank?
   end
 
   def vote(l_popular)
     content_tag(:p, link_to(l_popular.title, l_popular, class: 'pop-title')) +
-      image_tag(l_popular.image, class: 'pop-img')
+      image_tag(l_popular.image, class: 'pop-img') unless l_popular.image.blank?
+        
+      end
   end
-end
+
