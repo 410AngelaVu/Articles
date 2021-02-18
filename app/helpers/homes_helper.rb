408b0f85396c0d8fe48441804a40346f1c71b32a
410AngelaVu <andjela1993@gmail.com>
2021-02-18 18:11:54 +0100

    Added README file and fixed rubocop offenses

--- app/helpers/homes_helper.rb
+++ app/helpers/homes_helper.rb
@@ -1,6 +1,6 @@
 module HomesHelper
   def first_displayed_category
-    link_to(Category.find(1).articles.last.title, Category.find(1).articles.last, class: 'p-ti') 
+    link_to(Category.find(1).articles.last.title, Category.find(1).articles.last, class: 'p-ti')
   end
 
   def second_displayed_category
@@ -16,26 +16,27 @@ module HomesHelper
   end
 
   def first_displayed_image
-    image_tag(Category.find(1).articles.last.image, class: 'arti-img') unless Category.find(1).articles.last.image.blank?
-      
-    end
+    return unless Category.find(1).articles.last.image.attached?
+
+    image_tag(Category.find(1).articles.last.image, class: 'arti-img')
+  end
 
   def second_displayed_image
-    image_tag(Category.find(2).articles.last.image, class: 'arti-img') unless Category.find(2).articles.last.image.blank?
+    image_tag(Category.find(2).articles.last.image, class: 'arti-img') if Category.find(2).articles.last.image.attached?
   end
 
   def third_displayed_image
-    image_tag(Category.find(3).articles.last.image, class: 'arti-img') unless Category.find(3).articles.last.image.blank?
+    image_tag(Category.find(3).articles.last.image, class: 'arti-img') if Category.find(3).articles.last.image.attached?
   end
 
   def fourth_displayed_image
-    image_tag(Category.find(4).articles.last.image, class: 'arti-img') unless Category.find(4).articles.last.image.blank?
+    image_tag(Category.find(4).articles.last.image, class: 'arti-img') if Category.find(4).articles.last.image.attached?
   end
 
   def vote(l_popular)
-    content_tag(:p, link_to(l_popular.title, l_popular, class: 'pop-title')) +
-      image_tag(l_popular.image, class: 'pop-img') unless l_popular.image.blank?
-        
-      end
-  end
+    return unless l_popular.image.attached?
 
+    content_tag(:p, link_to(l_popular.title, l_popular, class: 'pop-title')) +
+      image_tag(l_popular.image, class: 'pop-img')
+  end
+end
