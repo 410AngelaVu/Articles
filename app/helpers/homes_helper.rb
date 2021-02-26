b6d9a4db63435c8c5938a203fa7b655329d10a06
410AngelaVu <andjela1993@gmail.com>
2021-02-26 22:32:08 +0100

    Added database

--- app/helpers/homes_helper.rb
+++ app/helpers/homes_helper.rb
@@ -1,22 +1,22 @@
 module HomesHelper
   def first_displayed_category
     link_to(Category.first&.articles&.last&.title, Category.first&.articles&.last,
-            class: 'p-ti')
+            class: 'p-ti') unless Category.first&.articles&.last&.title.blank?
   end
 
   def second_displayed_category
     link_to(Category.second&.articles&.last&.title, Category.second&.articles&.last,
-            class: 'p-ti')
+            class: 'p-ti') unless Category.second&.articles&.last&.title.blank?
   end
 
   def third_dipalyed_category
     link_to(Category.third&.articles&.last&.title, Category.third&.articles&.last,
-            class: 'p-ti')
+            class: 'p-ti') unless Category.third&.articles&.last&.title.blank?
   end
 
   def fourth_displayed_category
     link_to(Category.fourth&.articles&.last&.title, Category.fourth&.articles&.last,
-            class: 'p-ti')
+            class: 'p-ti') unless Category.fourth&.articles&.last&.title.blank?
   end
 
   def first_displayed_image
@@ -41,6 +41,6 @@ module HomesHelper
 
   def vote(l_popular)
     content_tag(:p, link_to(l_popular.title, l_popular, class: 'pop-title')) +
-      image_tag(l_popular.image, class: 'pop-img')
+      image_tag(l_popular.image, class: 'pop-img') if l_popular.image.present?
   end
 end
